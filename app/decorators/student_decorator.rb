9e4c092c3385f57bd5bd93d924130233f897a36d
Kamil <kamil.filip94@gmail.com>
2017-07-09 23:22:52 +0200

    Fixed student & teacher decorators

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -3,6 +3,11 @@ class StudentDecorator < BaseDecorator
     "#{first_name} #{last_name}"
   end
 
-  def avg_notes(subject_item)
-  end
+	def avg_notes(subject_item)
+	unless subject_item_notes.where(student_id: id).where(subject_item_id: subject_item.id).present?
+		'0.00'
+	else
+		sprintf('%.2f', subject_item_notes.where(student_id: id).where(subject_item_id: subject_item.id).average(:value).round(2)).to_s
+	end
+end
 end
