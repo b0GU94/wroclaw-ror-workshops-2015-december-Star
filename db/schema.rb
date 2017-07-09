ef65f476e5f9d50e6a5224f45b4a8de1971ae828
Kamil <kamil.filip94@gmail.com>
2017-07-10 00:08:37 +0200

    Added birthdate column to Student model

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20150719115626) do
     t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+		t.date "birthdate"
+
   end
 
   create_table "subject_item_notes", force: :cascade do |t|
