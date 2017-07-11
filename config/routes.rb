7c2782d4e8b1545c6c937b50297fa5dbfa0562a3
Kamil <kamil.filip94@gmail.com>
2017-07-11 23:34:05 +0200

    Fixed bug with '/user/sign_out route & added generating payments in seeds.rb & added birthdate field in separate migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
 
   devise_for :users
+
+	devise_scope :user do
+	 get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
 	root 'visitors#index'
 
   resources :students do
     get :subjects
   end
 
-
 	resources :payments do
-		
 	end
-
 
 	resources :teachers do
 		get :subjects
--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,15 @@ end
   )
 end
 
+35.times do
+  Payment.create!(
+    month: rand(1..12),
+    year: rand(2015..2017),
+		student_id: rand(1..25),
+		date_of_payment: Faker::Date.between(2.years.ago, Date.today),
+  )
+end
+
 students = Student.all
 SubjectItem.all.each do |subject_item|
   subject_item.students << students.sample(rand(1..4))
